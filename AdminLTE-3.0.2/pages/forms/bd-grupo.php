dc224777a968fa0f87c2fe3534c1890055404186
lizbeth-pozos-zey <56325021+lizbeth-pozos-zey@users.noreply.github.com>
2020-04-06 03:28:18 -0500

    Listar
    Lizbeth

--- AdminLTE-3.0.2/pages/forms/bd-grupo.php
+++ AdminLTE-3.0.2/pages/forms/bd-grupo.php
@@ -128,11 +128,11 @@ include ('conexionprueba.php');
                   <?php 
                   if(isset($_GET['aksi']) == 'delete'){
                          $nik = mysqli_real_escape_string($con,(strip_tags($_GET["nik"],ENT_QUOTES)));
-                         $cek = mysqli_query($con, "SELECT * FROM datos3 WHERE codigo='$nik'");
+                         $cek = mysqli_query($con, "SELECT * FROM datos3 WHERE grupo='$nik'");
                      if(mysqli_num_rows($cek) == 0){
                              echo '<div class="alert alert-info alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> No se encontraron datos.</div>';
                                  }else{
-                             $delete = mysqli_query($con, "DELETE FROM datos3 WHERE codigo='$nik'");
+                             $delete = mysqli_query($con, "DELETE FROM datos3 WHERE grupo='$nik'");
                           if($delete){
                               echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> Datos eliminado correctamente.</div>';
                                           }else{
